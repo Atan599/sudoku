59c74eaf21d63c9d6cc1e2008d5443a8a15fd15c
Atan599 <58374514+Atan599@users.noreply.github.com>
2023-05-16 19:52:03 +0200

    experiment

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ class square{
         this.x=x;
         this.y=y;
         this.value=0;
+        this.finalValue=0;
         this.posibilities=posibilities;
     }
 }
@@ -49,6 +50,7 @@ function createSudoku(){
             let target=lowests[randomInt(0,lowests.length-1)];
             target.value=target.posibilities[randomInt(0,target.posibilities.length-1)];
             let num=target.value;
+            target.finalValue=num;
             for(let j=0;j<9;j++){
                 
                let arr= table[j][target.y].posibilities;
@@ -89,13 +91,59 @@ function sudokuTable(){
     var out=document.getElementById("sudoku");
     var outstring="<table class='tabulkaSudoku'>";
     var table=createSudoku();
+    celarSudoku(table);
     for(let i=0;i<9;i++){
         outstring+="<tr>";
         for(let j=0;j<9;j++){
-            outstring+="<td class='bunkaSudoku "+(((Math.floor(i/3)+(Math.floor(j/3)%2!=0?1:0))%2==0)?"ctverecLichy":"ctverecSudy")+"'>"+table[i][j].value+"</td>";
+            outstring+="<td class='bunkaSudoku "+(((Math.floor(i/3)+(Math.floor(j/3)%2!=0?1:0))%2==0)?"ctverecLichy":"ctverecSudy")+"'>"+table[i][j].finalValue+"</td>";
         }
         outstring+="</tr>";
     }
     outstring+="</table>";
     out.innerHTML=outstring;
+}
+function removeSudokuTile(table,tile){
+    tile.finalValue=0;
+    target=tile;
+    let num=tile.value;
+    for(let j=0;j<9;j++){
+                
+        let arr= table[j][target.y].posibilities;
+        if(arr.indexOf(num)==-1){
+            arr.push(num);
+           
+          }
+
+       arr= table[target.x][j].posibilities;
+       if(arr.indexOf(num)==-1){
+        arr.push(num);
+       
+      }
+       arr=subSquare(target.x,target.y,j,table);
+       if(arr.indexOf(num)==-1){
+         arr.push(num);
+        
+       }
+     }
+}
+function celarSudoku(table){
+    let can=true;
+    while(can){
+        let zeros=[];
+        for(let i=0;i<9;i++){
+            for(let j=0;j<9;j++){
+                var actual=table[i][j];
+                if(actual.posibilities.length==0){
+                    zeros.push(actual);
+                }
+            }
+        }
+        if(zeros.length>0){
+            removeSudokuTile(table,zeros[randomInt(0,zeros.length-1)]);              
+        }else{
+            can=false;
+        }
+ 
+    }
+
 }
