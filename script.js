eb2c0f4759a8da2ed777e2c4815b84947987435a
atan599 <kmartan06@gmail.com>
2023-05-26 07:47:16 +0200

    seed

--- script.js
+++ script.js
@@ -11,6 +11,8 @@ var dificulty=81;
 var actualTable;
 var images=["Logo.jpg","Slide1.jpg","Slide2.jpg"];
 var actualImage=0;
+var seed=undefined;
+var random;
 function createSudoku(){
     let table;
     while(true){
@@ -82,7 +84,13 @@ function createSudoku(){
     return table;
 }
 function randomInt(min,max){
-    return Math.floor(Math.random()*(max-min+1)+min);
+    if(seed==undefined){
+        return Math.floor(Math.random()*(max-min+1)+min);   
+    }
+    else{
+        return Math.floor(random()*(max-min+1)+min);
+    }
+
 }
 function subSquare(x,y,index,table){
     let sqrx=Math.floor(x/3);
@@ -112,6 +120,7 @@ function sudokuTable(){
     actualTable=table;
     logSudoku(table);
     generateMatrix();
+    loadSeed();
 }
 
 
@@ -243,3 +252,45 @@ function slideshow(){
    out.src="./Images/"+images[(actualImage++)%images.length];
    setTimeout(slideshow,5000);
 }
+function loadSeed(){
+    var value=document.getElementById("seed").value;
+    if(value!=""){
+        seed=cyrb128(value);
+    random=sfc32(seed[0],seed[1],seed[2],seed[3]);    
+    }else{
+        seed=undefined;
+        random=undefined;
+    }
+
+}
+//zde začíná skopírovaný kód
+function cyrb128(str) {
+    let h1 = 1779033703, h2 = 3144134277,
+        h3 = 1013904242, h4 = 2773480762;
+    for (let i = 0, k; i < str.length; i++) {
+        k = str.charCodeAt(i);
+        h1 = h2 ^ Math.imul(h1 ^ k, 597399067);
+        h2 = h3 ^ Math.imul(h2 ^ k, 2869860233);
+        h3 = h4 ^ Math.imul(h3 ^ k, 951274213);
+        h4 = h1 ^ Math.imul(h4 ^ k, 2716044179);
+    }
+    h1 = Math.imul(h3 ^ (h1 >>> 18), 597399067);
+    h2 = Math.imul(h4 ^ (h2 >>> 22), 2869860233);
+    h3 = Math.imul(h1 ^ (h3 >>> 17), 951274213);
+    h4 = Math.imul(h2 ^ (h4 >>> 19), 2716044179);
+    return [(h1^h2^h3^h4)>>>0, (h2^h1)>>>0, (h3^h1)>>>0, (h4^h1)>>>0];
+}
+function sfc32(a, b, c, d) {
+    return function() {
+      a >>>= 0; b >>>= 0; c >>>= 0; d >>>= 0; 
+      var t = (a + b) | 0;
+      a = b ^ b >>> 9;
+      b = c + (c << 3) | 0;
+      c = (c << 21 | c >>> 11);
+      d = d + 1 | 0;
+      t = t + d | 0;
+      c = c + t | 0;
+      return (t >>> 0) / 4294967296;
+    }
+}
+//zde končí skopírovaný kód
